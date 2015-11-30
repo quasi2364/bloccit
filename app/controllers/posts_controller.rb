4b3f8e25c624ebf712c1ed99126183ed1bffae7c
Dan Levin <dlevin64@gmail.com>
2015-11-29 19:16:55 -0500

    Checkpoint 23

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,16 @@
 class PostsController < ApplicationController
+
   def index
   	@posts = Post.all
   end
 
   def show
   	@post = Post.find(params[:id])
+    #@post.title = params[:post][:title]
+    #@post.body = params[:post][:body]
   end
 
-  def new
+  def new # views/posts/new.html.erb
   	@post = Post.new
   end
 
@@ -26,5 +29,31 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    if @post.save
+      flash[:notice] = "Post was updated."
+      redirect_to @post
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
+      flash[:notice] = "\"#{@post.title}\" was deleted succesfully"
+      redirect_to posts_path
+    else
+      flash[:error] = "There was an error deleting the post."
+      render :show
+    end
   end
 end
