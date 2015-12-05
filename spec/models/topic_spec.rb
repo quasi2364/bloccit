9cbbd0489a7d1cc2af2ef7aa0275b656b9bd4962
Dan Levin <dlevin64@gmail.com>
2015-12-05 15:46:38 -0500

    Assignment 35

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -4,7 +4,11 @@ include RandomData
 RSpec.describe Topic, type: :model do
   let(:topic) {Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)}
 
-  it {should have_many(:posts)}
+  it { should have_many(:posts) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:description) }
+  it { should validate_length_of(:name).is_at_least(5) }
+  it { should validate_length_of(:description).is_at_least(15) }
   
   describe "attributes" do
   	it "responds to name" do
