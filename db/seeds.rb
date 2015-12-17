debaa72a00cbde4236ab264e87c4e8c90eeb1c82
Dan Levin <dlevin64@gmail.com>
2015-12-16 22:54:09 -0500

    Checkpoint 42

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,6 +35,29 @@ Comment.create!(
 )
 end
 
+25.times do
+	Label.create!(
+		name: RandomData.random_word
+	)
+end
+labels = Label.all
+
+100.times do
+	Labeling.create!(
+		label: labels.sample,
+		labelable: topics.sample,
+		labelable_type: "Topic"
+	)
+end
+
+100.times do
+	Labeling.create!(
+		label_id: rand(0..24),
+		labelable_id: rand(0..49),
+		labelable_type: "Post"
+	)
+end
+
 #Create an admin user
 admin = User.create!(
 	name: "Admin User",
@@ -55,3 +78,5 @@ puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Label.count} labels created"
+puts "#{Labeling.count} labelings created"
