0c64d9d6964d496b1dc308e2915b205b888930c5
Dan Levin <dlevin64@gmail.com>
2015-12-24 16:46:39 -0500

    Checkpoint 46

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -27,4 +27,22 @@ RSpec.describe Topic, type: :model do
   		expect(topic.public).to be(true)
   	end
   end
+
+  describe "scopes" do
+    before do
+      @public_topic = create(:topic)
+      @private_topic = create(:topic, public: false)
+    end
+
+    describe "visible_to(user)" do
+      it "returns all topics if the user is present" do
+        user = User.new
+        expect(Topic.visible_to(user)).to eq(Topic.all)
+      end
+
+      it "returns only public topics if user is nil" do
+        expect(Topic.visible_to(nil)).to eq([@public_topic])
+      end
+    end
+  end
 end
