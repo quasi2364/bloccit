657732766da850c31ac82a2c0b18f51c26694719
Dan Levin <dlevin64@gmail.com>
2015-12-28 14:55:11 -0500

    Checkpoint 48

---
+++ db/migrate/20151227205920_add_authentication_token_to_user.rb
@@ -0,0 +1,5 @@
+class AddAuthenticationTokenToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :auth_token, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151219230011) do
+ActiveRecord::Schema.define(version: 20151227205920) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -117,6 +117,7 @@ ActiveRecord::Schema.define(version: 20151219230011) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "role"
+    t.string   "auth_token"
   end
 
   create_table "votes", force: :cascade do |t|
