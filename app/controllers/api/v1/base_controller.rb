5ff64b883905278bd7ad6c358efea4db02a96888
Dan Levin <dlevin64@gmail.com>
2016-01-02 12:20:23 -0500

    Assignment 49

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::BaseController < ApplicationController
+	include SessionsHelper
+	
 	skip_before_action :verify_authenticity_token
 
 	rescue_from ActiveRecord::RecordNotFound, with: :not_found
---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,46 @@
+class Api::V1::PostsController < Api::V1::BaseController
+	include SessionsHelper
+
+	before_action :authenticate_user, except: [:index, :show]
+	before_action :authorize_user, except: [:index, :show]
+
+	def update
+		post = Post.find(params[:id])
+
+		if post.update_attributes(post_params)
+			render json: post.to_json, status: 200
+		else
+			render json: {error: "Post not updated", status: 400}, status: 400
+		end
+	end
+
+	def destroy
+		post = Post.find(params[:id])
+
+		if post.destroy
+			render json: {message: "Post destroyed", status: 200}, status: 200
+		else
+			render json: {error: "Post wasn't destroyed successfully", status: 400}, status: 400
+		end
+	end
+
+	def create
+		topic = Topic.find(params[:topic_id])
+		post = topic.posts.new(post_params)
+
+		if post.valid?
+			post.save!
+			render json: post.to_json, status: 201
+		else
+			render json: {error: "Post was not created", status: 400}, status: 400
+		end
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :body, :user_id)
+	end
+end
+
+
